de973923f690c4dd09c39ab7de0609bfab4bca1f
mihir-n25 <mihirnebani@gmail.com>
2024-06-30 13:02:41 +0530

    AI is ready

--- server/routes/dalle.route.js
+++ server/routes/dalle.route.js
@@ -8,11 +8,30 @@ dotenv.config();
 const router = express.Router();
 
 const openai = new OpenAI({
-    apiKey: process.env,
+    apiKey: process.env.OPENAI_API_KEY,
   });
 
 router.route('/').get((req,res) => {
     res.status(200).json({message : "hello from DallE routes"})
+})
+
+router.route('/').post(async(req,res) => {
+    try {
+        const {prompt} = req.body;
+
+        const response = await openai.images.generate({
+            prompt : prompt,
+            n:1,
+            size : "1024x1024",
+            response_format : "b64_json"
+        });
+
+        const image = response.data.data[0].b64_json;
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message : "Something Went Wrong"})
+    }
 })
 
 export default router;
