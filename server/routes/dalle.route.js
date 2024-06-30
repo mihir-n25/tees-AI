894ea8ddeb7720050db2fe6431f47de57b5d9465
mihir-n25 <mihirnebani@gmail.com>
2024-06-30 12:16:02 +0530

    again conflicted

--- server/routes/dalle.route.js
+++ server/routes/dalle.route.js
@@ -1,11 +1,15 @@
 import express from "express"
 
 import * as dotenv from "dotenv";
-// import { OpenAIApi} from 'openai'
+import OpenAI from 'openai';
 
 dotenv.config();
 
 const router = express.Router();
+
+const openai = new OpenAI({
+    apiKey: process.env,
+  });
 
 router.route('/').get((req,res) => {
     res.status(200).json({message : "hello from DallE routes"})
