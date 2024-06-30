b3dc4dfca235208c47e377a75f32c444111fa466
mihir-n25 <mihirnebani@gmail.com>
2024-06-30 10:52:49 +0530

    ui updates

--- client/src/components/FilePicker.jsx
+++ client/src/components/FilePicker.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import CustomButton from './CustomButton'
 
-const FilePicker = () => {
+const FilePicker = ({file , setFile , readFile}) => {
   return (
-    <div>FilePicker</div>
+    <div className='filepicker-container'>
+      <div className='flex-1 flex flex-col'>
+        <input
+        id='file-upload'
+        type='file'
+        accept='image/*'
+        onChange={(e) => setFile(e.target.files[0])}
+        />
+      </div>
+      <label htmlFor='file-upload' className='filepicker-label'>
+        Upload File
+      </label>
+
+      <p className='mt-2 text-gray-500 text-sm truncate'>
+        {file === "" ? "No file selected" : file.name}
+      </p>
+      <div className='mt-4 flex flex-wrap gap-36'>
+        <CustomButton/>
+      </div>
+    </div>
   )
 }
 
