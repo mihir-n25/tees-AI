e6897f82672a04a8e33d1658aa41e1ddcb2260e9
mihir-n25 <mihirnebani@gmail.com>
2024-06-30 11:17:45 +0530

    file [ppicker done

--- client/src/components/FilePicker.jsx
+++ client/src/components/FilePicker.jsx
@@ -11,16 +11,28 @@ const FilePicker = ({file , setFile , readFile}) => {
         accept='image/*'
         onChange={(e) => setFile(e.target.files[0])}
         />
-      </div>
+
       <label htmlFor='file-upload' className='filepicker-label'>
         Upload File
       </label>
 
-      <p className='mt-2 text-gray-500 text-sm truncate'>
+      <p className='mt-2 text-gray-500 text-sm'>
         {file === "" ? "No file selected" : file.name}
       </p>
-      <div className='mt-4 flex flex-wrap gap-36'>
-        <CustomButton/>
+      <div className='mt-4 flex flex-wrap gap-3'>
+        <CustomButton
+        type={"outline"}
+        title={"Logo"}
+        handleClick={() => readFile("logo")}
+        customStyles={"text-xs"}
+        />
+        <CustomButton
+        type={"filled"}
+        title={"Full"}
+        handleClick={() => readFile("logo")}
+        customStyles={"text-xs"}
+        />
+      </div>
       </div>
     </div>
   )
