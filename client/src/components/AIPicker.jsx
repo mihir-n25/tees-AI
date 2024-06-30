c6e20788663aa5144e0be4a556e686cdb8c22b20
mihir-n25 <mihirnebani@gmail.com>
2024-06-30 10:24:05 +0530

    conflictssss

--- client/src/components/AIPicker.jsx
+++ client/src/components/AIPicker.jsx
@@ -1,34 +1,8 @@
 import React from 'react'
 
-import CustomButton from './CustomButton';
-const AIPicker = ({prompt , setPrompt , geeneratingImg , handleSubmit}) => {
+const AIPicker = () => {
   return (
-    <div className='aipicker-container'>
-      <textarea 
-      placeholder='Ask AI..'
-      rows={5}
-      value={prompt}
-      onChange={(e) => setPrompt(e.target.value)}
-      className='aipicker-textarea'
-      />
-      <div className='flex flex-wrap gap-3'>
-        {geeneratingImg ? (
-          <CustomButton 
-          type='outline'
-          title='Askin AI...'
-          customStyles="text-xs"
-          /> 
-        ) : (
-          <>
-          <CustomButton type='filled'
-          title="AI Full"
-          handleClick={() => handleSubmit("full")}
-          customStyles="text-xs"
-          />
-          </>
-        )}
-      </div>
-    </div>
+    <div>AIPicker</div>
   )
 }
 
--- client/src/components/FilePicker.jsx
+++ client/src/components/FilePicker.jsx
@@ -2,17 +2,7 @@ import React from 'react'
 
 const FilePicker = () => {
   return (
-    <div className='filepicker-container'>
-      <div className='flex-1 flex flex-col'>
-        <input
-        id='file-upload'
-        type='file'
-        accept='image/*'
-        onChange={(e) => setFile(e.target.files[0])}
-        />
-      </div>
-      
-    </div>
+    <div>FilePicker</div>
   )
 }
 
