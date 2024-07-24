fca43161efd01eb819c8caa7523540aff76258d3
mihir-n25 <mihirnebani@gmail.com>
2024-07-24 10:44:57 +0530

    reaady to deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,16 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   chunkSizeWarningLimit: 1000, // Adjust as needed
-  //   rollupOptions: {
-  //     output: {
-  //       manualChunks(id) {
-  //         if (id.includes('node_modules')) {
-  //           return 'vendor';
-  //         }
-  //       },
-  //     },
-  //   },
-  // },
+  build: {
+    base: '/teesAI/'
+  },
 })
