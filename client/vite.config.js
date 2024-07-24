7e5f046e62d4e4321ae9566736bec1f33558678f
mihir-n25 <mihirnebani@gmail.com>
2024-07-24 10:22:37 +0530

    bug resolved

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,17 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : '/tees-AI/',
-  build: {
-    chunkSizeWarningLimit: 1000, // Adjust as needed
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-      },
-    },
-  },
+  // build: {
+  //   chunkSizeWarningLimit: 1000, // Adjust as needed
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks(id) {
+  //         if (id.includes('node_modules')) {
+  //           return 'vendor';
+  //         }
+  //       },
+  //     },
+  //   },
+  // },
 })
